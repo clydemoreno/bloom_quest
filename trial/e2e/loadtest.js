fc4a41cadafa9e8d27df0ec9a0b6d5638a963a66
Clyde Moreno <49640401+clarencemoreno@users.noreply.github.com>
2023-09-14 09:44:22 +0800

    added env

--- trial/e2e/loadtest.js
+++ trial/e2e/loadtest.js
@@ -1,26 +1,26 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
 
+// Define the number of VUs using an environment variable
+const VUS = __ENV.VUS || 10;
+
 export const options = {
   stages: [
-    { duration: '30s', target: 10 }, // Ramp up to 10 VUs over 30 seconds
-    { duration: '1m', target: 10 },  // Stay at 10 VUs for 1 minute
-    { duration: '30s', target: 100 }, // Ramp up to 100 VUs over 30 seconds
-    { duration: '1m', target: 100 },  // Stay at 100 VUs for 1 minute
-    { duration: '30s', target: 0 },   // Ramp down to 0 VUs over 30 seconds
+    { duration: '10s', target: VUS }, // Ramp up to the specified number of VUs over 30 seconds
+    { duration: '1m', target: VUS }, // Stay at the specified number of VUs for 2 minutes (usebloom=1)
+    { duration: '1m', target: VUS }, // Stay at the specified number of VUs for 2 minutes (usebloom=0)
+    { duration: '10s', target: 0 },  // Ramp down to 0 VUs over 30 seconds
   ],
-  thresholds: {
-    http_req_duration: ['p(95)<500'], // Set a response time threshold
-  },
+//   thresholds: {
+//     http_req_duration: ['p(95)<500'], // Set a response time threshold
+//   },
 };
 
 export default function () {
-  // Your script logic here
-
-  const randomValue = Math.random();
-
   // Generate a random order ID based on the distribution
   let randomOrderId;
+  const randomValue = Math.random();
+
   if (randomValue <= 0.05) {
     // 5% of requests with order ID between 1 and 201
     randomOrderId = Math.floor(Math.random() * 201) + 1;
@@ -29,16 +29,25 @@ export default function () {
     randomOrderId = Math.floor(Math.random() * 9800) + 201;
   }
 
+  // Test with usebloom=1
+  if (__VU <= VUS) {
+    const urlWithBloom = `http://127.0.0.1:5000/orders/${randomOrderId}?usebloom=1`;
+    const responseWithBloom = http.get(urlWithBloom);
+    check(responseWithBloom, {
+      'is status 200': (r) => r.status === 200,
+      'is status 404': (r) => r.status === 404,
+    });
+    sleep(0.5);
+  }
 
-
-//   const randomOrderId = Math.floor(Math.random() * 9800) + 201;
-  const url = `http://127.0.0.1:5000/orders/${randomOrderId}?usebloom=0`;
-  const response = http.get(url);
-  check(response, {
-    'is status 200': (r) => r.status === 200,
-    'is status 404': (r) => r.status === 404,
-  });
-  sleep(0.5);
+  // Test with usebloom=0
+  if (__VU <= VUS) {
+    const urlWithoutBloom = `http://127.0.0.1:5000/orders/${randomOrderId}?usebloom=0`;
+    const responseWithoutBloom = http.get(urlWithoutBloom);
+    check(responseWithoutBloom, {
+      'is status 200': (r) => r.status === 200,
+      'is status 404': (r) => r.status === 404,
+    });
+    sleep(0.5);
+  }
 }
-
-
